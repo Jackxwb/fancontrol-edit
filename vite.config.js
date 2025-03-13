39300111f562ab64b9d01f0229138f813782bfe0
Jackxwb <34720403+Jackxwb@users.noreply.github.com>
2025-03-14 00:54:08 +0800

    修正Github page路径，删除编译缓存

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+
+  // github page
+  base: process.env.IS_GITHUB_ACTION === '1' ? '/fancontrol-edit/' : '/'
 })
